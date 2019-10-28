6330cbf02658d254154fa3be3f63884fff37adb0
Ekin Nohutçu <ekinohutcu@gmail.com>
2019-10-28 22:36:49 +0300

    Comments added by Ekin Nohutçu

--- src/Money.java
+++ src/Money.java
@@ -1,19 +1,27 @@
+//Money class.
 public class Money extends Player{
 
-    private int money;
+    private int money; //money amount
 
+    //Constructor of Money Class.
     public Money(int money){
+
         this.money = money;
     }
 
+    //getter method for money amount
     public int getMoney(){
+
         return this.money;
     }
 
+    //method for increase the amount of money with a given parameter.
     public void increaseMoney(int increase){
+
         this.money += increase;
     }
 
+    //method for decrease the amount of money with a given parameter.
     public void decreaseMoney(int decrease){
         this.money -= decrease;
     }
--- src/Piece.java
+++ src/Piece.java
@@ -1,16 +1,23 @@
+//Piece class.
 public class Piece extends Player{
 
-    private String pieceType;
+    private String pieceType; //type of the piece e.g. car.
 
+    //Constructor of Piece Class.
     public Piece(String pieceType){
+
         this.pieceType = pieceType;
     }
 
+    //getter method for piece.
     public String getPieceType() {
+
         return pieceType;
     }
 
+    //setter method for piece.
     public void setPieceType(String pieceType) {
+
         this.pieceType = pieceType;
     }
 }
