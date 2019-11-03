9729acf6180208a262ed95ca369cf13b51284136
Arda Bayram <arda.bayram98@hotmail.com>
2019-11-03 16:00:36 +0300

    Updated MonopolyGame and Board classes by Arda. I added little differences for our new abstract class to the Board class. Also, I changed MonopolyGame class. Because it had mistakes.

--- src/Board.java
+++ src/Board.java
@@ -4,18 +4,21 @@ public class Board extends MonopolyGame{
 
     private final int BOARD_SIZE = 40; //board size. it is fixed to 40.
     private Square[] squareList; //Square list to keep all squares on the board.
+    private int taxNumber;
+    private int taxValue;
+
+    public Board(){
+    }
 
     //Default constructor of Board Class.
-    public Board(){
-
+    public Board(int taxNumber,int taxValue){
         this.squareList = new Square[BOARD_SIZE]; //set the square list's size to board size.
-
-
+        this.taxNumber = taxNumber;
+        this.taxValue = taxValue;
     }
 
     //getter method for board size.
     public int getBoardSize(){
-
         return BOARD_SIZE;
     }
 
@@ -27,11 +30,30 @@ public class Board extends MonopolyGame{
     //set the square list
     public void setSquareList() {
 
-        for (int i = 0; i < BOARD_SIZE;i++)
+        int random, count = 0;
+        while(true)
         {
-            this.squareList[i] = new Square(i);
-        }
+            random = (int) (Math.random() * 39 + 1 );
 
+            if(count < taxNumber){
+                if(squareList[random] == null){
+                    squareList[random] = new TaxSquare(random,"TAX",taxValue);
+                    count++;
+                }
+            }
+            else {
+                for(int i = 0; i < BOARD_SIZE; i++){
+                    if(squareList[i] == null){
+                        squareList[i] = new NormalSquare(i,"NORMAL");
+                    }
+                }
+                break;
+            }
+        }
         this.squareList[0].setSquareName("GO"); //name the first square as GO square.
+
+        for (int i = 0; i < BOARD_SIZE; i++){
+            System.out.println("Square id: " +this.squareList[i].getSquareID() + " squarename: " +this.squareList[i].getSquareName() );
+        }
     }
 }
--- src/MonopolyGame.java
+++ src/MonopolyGame.java
@@ -5,6 +5,8 @@ public class MonopolyGame {
     private Player[] playerList; //Player array to create given number of players
     private int playerSize, taxValue, startMoney, taxNumber;
     private Dice dice;
+    private Piece[] pieceList;
+
 
     //Default constructor of MonopolyGame
     public MonopolyGame() {
@@ -16,7 +18,7 @@ public class MonopolyGame {
         this.taxValue = taxValue;
         this.startMoney = startMoney;
         this.taxNumber = taxNumber;
-        this.board = new Board();
+        this.board = new Board(taxNumber, taxValue);
         this.dice = new Dice();
     }
 
@@ -24,36 +26,79 @@ public class MonopolyGame {
     public void Play() {
 
         this.playerList = new Player[this.playerSize];
+        this.pieceList = new Piece[this.playerSize];
 
         board.setSquareList();
 
-        for (int i = 0; i < playerSize; i++){
-            playerList[i] = new Player(NAMES[i], PIECES[i], startMoney);
+        for (int i = 0; i < playerSize; i++) {
+            playerList[i] = new Player(NAMES[i], startMoney);
+            pieceList[i] = new Piece(PIECES[i], this.board);
+            playerList[i].setPiece(pieceList[i]);
         }
-        int diceValue;
-        int count = 0;
-        while(true){
-            for(int i = 0; i < playerSize; i++){
-                diceValue = dice.roll();
-                System.out.println(playerList[i].getPlayerName() + "'s old position : " + playerList[i].getPosition());
-                playerList[i].move(diceValue);
-                System.out.println("Dice is " + diceValue);
 
-                if(playerList[i].getPosition() >= 40){
-                    playerList[i].setMovePosition();
+        int diceValue;
+        int firstDice;
+        int secondDice;
+
+        for (int i = 0; i < playerSize; i++) {
+            System.out.print("Player : " + playerList[i].getPlayerName() + " -- ");
+            System.out.println("Piece: " + pieceList[i].getPieceType());
+        }
+
+        boolean check = true;
+        int currentPlayerSize = playerSize;
+        System.out.println("Current = " + currentPlayerSize);
+
+        while (check) {
+
+            System.out.println("----------------");
+            for (int i = 0; i < playerSize; ) {
+
+                if (playerList[i] == null) {
+                    i++;
+                    continue;
                 }
 
+                firstDice = dice.getFirstValue();
+                secondDice = dice.getSecondValue();
+                diceValue = firstDice + secondDice;
 
-                System.out.println(playerList[i].getPlayerName() + "'s position is " + playerList[i].getPosition());
+                System.out.println("\nCurrent players: " + currentPlayerSize);
 
+                System.out.println("\n" + playerList[i].getPlayerName() + "'s money: " + playerList[i].getMoney().getCurrentMoney());
+                System.out.println(playerList[i].getPlayerName() + "'s old position : " + playerList[i].getPiece().getSquare().getSquareID());
+                playerList[i].getPiece().move(diceValue);
+                System.out.println("Dice is " + diceValue);
 
-            }
-            count++;
-            if(count == 10) {
-                break;
+                System.out.println(playerList[i].getPlayerName() + "'s position is " + playerList[i].getPiece().getSquare().getSquareID());
+
+                if (playerList[i].getPiece().getSquare().getSquareName() == "TAX") {
+                    playerList[i].getMoney().decreaseMoney(taxValue);
+                    System.out.println(playerList[i].getPlayerName() + "'s money: " + playerList[i].getMoney().getCurrentMoney());
+                    if (playerList[i].getMoney().getCurrentMoney() <= 0) {
+                        playerList[i] = null;
+                        currentPlayerSize--;
+
+                        if (currentPlayerSize == 1) {
+                            check = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (firstDice == secondDice) {
+                    System.out.println("First dice is : " + firstDice + " Second dice is : " + secondDice);
+                    continue;
+                } else {
+                    i++;
+                }
             }
         }
 
+        for (int i = 0; i < playerSize; i++) {
+            if (playerList[i] != null) {
+                System.out.println("\nWINNER : " + playerList[i].getPlayerName());
+            }
+        }
     }
-
 }
