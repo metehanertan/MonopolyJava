bcb6ae808515a08cce850ba98a7cd7c12e9a9ff7
Minel SAYGISEVER <minelsaygisever@gmail.com>
2019-12-06 21:34:10 +0300

    A little change was made in Board class and testPropertySquare class added.

---
+++ UnitTest/testPropertySquare.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class testPropertySquare {
+
+    PropertySquare ps = new PropertySquare(15, "KADIKÖY", 3,"Blue", 6);
+    Player player = new Player("Minel", 200);
+
+
+    @Test
+    public void testOwnerPlayer(){
+        ps.setOwner(player);
+        assertEquals("Error occured while setting owner.", ps.getOwner(), player);
+    }
+
+    @Test
+    public void testHasOwner(){
+        ps.setOwner(player);
+        assertTrue("Error occured while setting owner.", ps.getHasOwner());
+    }
+
+}
